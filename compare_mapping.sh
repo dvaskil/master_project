c0857f23475e0bcdeb6a02c2557d1f4b8d473519
Ubuntu <eskil-uh-iass>
2019-10-26 14:21:55 +0200

    Adding comments and tweaking prompt messages

--- compare_mapping.sh
+++ compare_mapping.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-echo "Input start range: "
-read start_range
-
-echo "Input max range: "
-read max_range
-
 echo "Input coverage: "
 read coverage
+
+echo "Input start error range: "
+read start_range
+
+echo "Input max error range: "
+read max_range
 
 echo "Input error rate step: "
 read err_step
 
+#Make file with rates
 printf "Matching mapping accuracy with increasing error rate. Coverage: $coverage\nErrRate\tMatch\n" > error_match$coverage.txt
 for i in $(seq $start_range $err_step $max_range)
 do
     numpy_alignments get_correct_rates truth_c${coverage}_e${i} bwa_c${coverage}_e${i} | awk 'FNR == 2 {print '$i' "\t" $2}' >> error_match$coverage.txt
 done
+
+#Print rates on screen
 clear
 cat error_match$coverage.txt
