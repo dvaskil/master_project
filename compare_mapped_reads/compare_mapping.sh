f7c69bf6376c8c4c28a515c45d4c2d3a60f13d6a
Ubuntu <eskil-uh-iass>
2019-10-30 16:49:39 +0100

    Started on readme and restructuring directories

--- compare_mapped_reads/compare_mapping.sh
+++ compare_mapped_reads/compare_mapping.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+'''
 echo "Input coverage: "
 read coverage
 
@@ -11,12 +11,28 @@ read max_range
 
 echo "Input error rate step: "
 read err_step
-
+'''
 #Make file with rates
+
 printf "Matching mapping accuracy with increasing error rate. Coverage: $coverage\nErrRate\tMatch\n" > error_match$coverage.txt
+"""
 for i in $(seq $start_range $err_step $max_range)
 do
     numpy_alignments get_correct_rates truth_c${coverage}_e${i} bwa_c${coverage}_e${i} | awk 'FNR == 2 {print '$i' "\t" $2}' >> error_match$coverage.txt
+done
+"""
+
+for files in .
+do
+    if [[ $files == *.npz ]]; then
+	start_range=${files[1]} | grep
+	max_range=${files[-1]} | grep
+	err_step=${files[2]} - $start_range | grep
+	for i in $(seq $start_range $err_step $max_range)
+	do
+	    numpy_alignments get_correct_rates truth_c${coverage}_e${i} bwa_c${coverage}_e${i} | awk 'FNR == 2 {print '$i' "\t" $2}' >> error_match$coverage.txt
+	done
+    fi
 done
 
 #Print rates on screen
