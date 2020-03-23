00093eaa692444e715866997836d6e83c2c5b96a
Eskil Vevik <dvaskil@gmail.com>
2020-03-23 13:20:20 +0100

    Updated scripts for more options support and the new graph_read_simulator

--- job_scripts/vg_map_compare.sh
+++ job_scripts/vg_map_compare.sh
@@ -6,8 +6,12 @@ Input is as follows:
 2. substitusion error rate of the simulated reads
 3. FASTA file of the simulated reads (.fa)
 4. File containing the positions of the simulated reads (.pos)
-5. Output directory where the result of the mapping accuracy is stored (map_acc.tsv)
+5. Output directory where the result of the mapping accuracy is stored
 6. Name of dir containing vg graphs for naming the outfile
+7. to include/exclude variants: "all", "variants" or "nonvariants"
+8. MapQ score threshold/cutoff
+9. ID number of sample
+10. masked or not
 '
 coverage=$1
 err=$2
@@ -15,11 +19,16 @@ simread=$3
 simposi=$4
 outdir=$5
 graphdir=$6
+var=$7
+mapq=$8
+sample_id=$9
+mask=${10}
+
+if [ -z "$mask" ]; then mask="both_"; fi
+printf "\nmask: $mask\n"
 
 filename=_c${coverage}_e${err}
-outfile=$outdir/${graphdir}acc$filename.tsv
-
-command > out 2>err
+outfile=$outdir/${mask}${var}_${sample_id}_${graphdir}${filename}_mapq${mapq}.tsv
 
 printf "\nsimread $simread\nsimposi $simposi\n outdir $outdir"
 printf "Matching mapping accuracy with vg map. Coverage: $coverage\nErrRate\tMatch\n" > $outfile
@@ -38,7 +47,7 @@ printf '\nNUMPY ALIGN TRUTH\n\n'
 cat $simposi | numpy_alignments store truth $outdir/truth$filename $reads
 
 printf "\nGETTING CORRECT RATES\n"
-numpy_alignments get_correct_rates $outdir/truth$filename $outdir/vggraph$filename >> $outfile # | awk 'FNR == 1 {print '$err' "\t" $2}' >> $outfile
+numpy_alignments get_correct_rates $outdir/truth$filename $outdir/vggraph$filename $7 -m $mapq >> $outfile # | awk 'FNR == 1 {print '$err' "\t" $2}' >> $outfile
 
 cat $outfile
 cp $outfile $HOME/project
